00bcca4d56a71bb17b254e4ea56ae19368cc479c
Jacob Garber <jgarber1@ualberta.ca>
2020-07-25 15:19:34 -0600

    Update board sizes
    Change the default board size to 5, and update the tested board sizes
    to the ones we need for geodesic Y.

--- open_spiel/games/geodesic_y_test.cc
+++ open_spiel/games/geodesic_y_test.cc
@@ -23,23 +23,23 @@ namespace {
 namespace testing = open_spiel::testing;
 
 void BasicGeodesicYTests() {
-  testing::LoadGameTest("geodesic_y(base_size=9)");
-  testing::NoChanceOutcomesTest(*LoadGame("geodesic_y(base_size=9)"));
+  testing::LoadGameTest("geodesic_y(base_size=5)");
+  testing::NoChanceOutcomesTest(*LoadGame("geodesic_y(base_size=5)"));
 
   testing::RandomSimTest(*LoadGame("geodesic_y"), 10);
 
   // All the sizes we care about.
-  for (int i = 5; i <= 26; i++) {
+  for (int i = 2; i <= 20; i++) {
     testing::RandomSimTest(*LoadGame(absl::StrCat("geodesic_y(base_size=", i, ")")),
                            10);
   }
 
   // Ansi colors!
   testing::RandomSimTest(
-      *LoadGame("geodesic_y", {{"base_size", GameParameter(9)},
+      *LoadGame("geodesic_y", {{"base_size", GameParameter(5)},
                       {"ansi_color_output", GameParameter(true)}}),
       1);
-  testing::RandomSimTest(*LoadGame("geodesic_y(base_size=10,ansi_color_output=True)"),
+  testing::RandomSimTest(*LoadGame("geodesic_y(base_size=5,ansi_color_output=True)"),
                          3);
 }
 
